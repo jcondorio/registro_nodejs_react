8b8dd03b644b54505c40a9e2974551119212f2a7
jcondorio <78916447+jcondorio@users.noreply.github.com>
2023-09-10 02:04:46 -0500

    point

---
+++ cliente_register/src/App.js
@@ -0,0 +1,12 @@
+
+import { Box, Button, Container } from "@mui/material"
+export default function App() {
+  return (
+    <Container maxWidth>
+      <Box>
+        aver
+      </Box>
+      <Button variant="contained">miprimer boton</Button>
+    </Container>
+  );
+}
